0f5bfcfa5b7f610f4dc2112ebb1e2c50d851c437
idevemohan <sharewithdeve@gmail.com>
2025-09-13 19:34:47 +0530

    Update App.jsx

--- frontend/bulkmail/src/App.jsx
+++ frontend/bulkmail/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   function send()
   {
     setStatus(true)
-     axios.post("https://bulkmail-backend-r1jn.onrender.com",{msg:msg,emailList:emailList})
+     axios.post("https://bulkmail-back.onrender.com",{msg:msg,emailList:emailList})
      .then(function(data)
     {
       if(data.data === true )
